0d2a317365e7f0fec80af502960df0de873462aa
mjafarshidik <mjafarshidik86@gmail.com>
2022-03-01 11:38:32 +0700

    Implement WebView

--- app/src/main/java/com/mjafarshidik/firsttaskchapter3binarbootcamp/MainActivity.kt
+++ app/src/main/java/com/mjafarshidik/firsttaskchapter3binarbootcamp/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.mjafarshidik.firsttaskchapter3binarbootcamp
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    private var webView: WebView? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webView)
+        webViewSetup()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    fun webViewSetup(){
+        webView!!.webViewClient = WebViewClient()
+        webView!!.loadUrl("https://www.binaracademy.com/")
+        val webSettings = webView!!.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.allowContentAccess = true
+        webSettings.allowFileAccess = true
+    }
+
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()) {
+            webView!!.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
